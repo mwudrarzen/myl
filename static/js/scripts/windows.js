ccd2afc210cc3a0447f670b2114057604d90c3ff
mwudrarzen <mwudrarzen@gmail.com>
2016-03-31 16:41:22 -0300

    310320162142

--- static/js/scripts/windows.js
+++ static/js/scripts/windows.js
@@ -1,9 +1,11 @@
-define([], function () {
+define(['jqueryui', 'scripts/utilities'], function (jQueryUI, utilitiesModule) {
 	console.log('iniciando windows.js');
 
-	var obj = {
+	/*var obj = {
+		currentZ: 20,
 		windowBase: $('#core-windowBase').html(),
 		openWindow: function (_windowId, _windowTitle, _windowContentElementId) {
+			var self = this;
 			var newWindow = $('<div id="' + _windowId.substring(1) + '" class="core-window"></div>');
 			newWindow.appendTo('#core-temporalElements');
 			newWindow.html(this.windowBase);
@@ -14,10 +16,37 @@ define([], function () {
 				top: $(document).height() / 2 - $(_windowContentElementId).height() / 2 - 51
 			});
 			$(_windowContentElementId).show();
+			newWindow.find('.core-window-container').draggable({
+			});
+			newWindow.bind({
+				click: function () {
+					self.currentZ += 1;
+					$(this).find('.core-window-container').zIndex(self.currentZ);
+				}
+			});
 		},
 		closeWindow: function (_windowId, _windowContentElementId) {
 			$(_windowContentElementId).appendTo('#core-hiddenElements');
 			$(_windowContentElementId).hide();
+			$(_windowId).remove();
+		}
+	};*/
+
+	//
+
+	var obj = {
+		windowHTML: $('#core-windowHTML').html(),
+		openWindow: function (_windowId, _windowTitle, _windowContentElementId) {
+			var newWindow = $('<div id="' + _windowId.substring(1) + '"></div>');
+			newWindow.html(this.windowHTML);
+			newWindow.find('.core-window-head-title').html(_windowTitle);
+			newWindow.find('.core-window-content').append($(_windowContentElementId));
+			newWindow.appendTo('#core-temporalElements');
+			$(_windowContentElementId).show();
+
+		},
+		closeWindow: function (_windowId) {
+			$(_windowId).find('.core-window-content:first-child').appendTo('#core-hiddenElements').hide();
 			$(_windowId).remove();
 		}
 	};
